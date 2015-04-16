5cd941a93d2f597d41606fdb028eeba1e9b7637a
shigemk2 <i.am.shige@gmail.com>
2015-04-16 18:14:50 +0900

    Fix 1-25

--- chapter01/1-25.c
+++ chapter01/1-25.c
@@ -1,11 +1,15 @@
 void function(int n) {
     int i, j, k , count = 0;
-    for(i = n/2; i <= n; i++)
-        for(j = 1; j + n/2 <= n; j = j + 1)
-            for(k = 1; k <= n; k = k * 2)
+    for(i = n/2; i <= n; i++) {
+        for(j = 1; j + n/2 <= n; j = j + 1) {
+            for(k = 1; k <= n; k = k * 2) {
                 count++;
+                printf("i: %d, j: %d, k: %d, count: %d\n", i, j, k, count);
+            }
+        }
+    }
 }
 
 main() {
-    function(100000);
+    function(8);
 }
