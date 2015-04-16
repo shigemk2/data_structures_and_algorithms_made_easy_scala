aa7f5dbe0907002372b19cdc511d1ede470da7ee
shigemk2 <i.am.shige@gmail.com>
2015-04-16 17:13:09 +0900

    Fix 1-23

--- chapter01/1-23.scala
+++ chapter01/1-23.scala
@@ -1,10 +1,14 @@
-def Function(n: Int): Unit = {
-  var i = 1;
-  var s = 1;
-  while(s <= n) {
-    i = i + 1
-    s = s + i
-    printf("*")
+object one23 {
+  def Function(n: Int): Unit = {
+    var i = 1;
+    var s = 1;
+    while(s <= n) {
+      i = i + 1
+      s = s + i
+      printf("*")
+    }
+  }
+  def main(args: Array[String]) {
+    Function(5)
   }
 }
-Function(5)
