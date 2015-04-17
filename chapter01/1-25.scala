313c2d903d3e8fbae031a736c72599a3a0c6dcf0
shigemk2 <i.am.shige@gmail.com>
2015-04-17 21:21:11 +0900

    Fix 1-25 and pow

--- chapter01/1-25.scala
+++ chapter01/1-25.scala
@@ -1,7 +1,7 @@
 object one25 {
   def Function(n: Int): Unit = {
     var count: Int = 0
-    for(i <- n/2 to n; j <- 1 to n if j + n/2 <= n; k <- 1 to n if (k == 1 || k % 2 == 0)) {
+    for(i <- n/2 to n; j <- 1 to n if j + n/2 <= n; k <- Iterator.iterate(1)(_ * 2) takeWhile (_ <= n)) {
       count = count + 1
       printf("i: %d, j: %d, k: %d, count: %d\n", i, j, k, count)
     }
--- chapter01/pow.scala
+++ chapter01/pow.scala
@@ -1,5 +1,3 @@
-var j=1
-for(i<-1.to(100).by(scala.math.pow(5,j).toInt)) {
-  println(i+" "+j)
-  j=j+1
+for(i <- Iterator.iterate(1)(_ * 2) takeWhile (_ <= 8)) {
+  println(i)
 }
