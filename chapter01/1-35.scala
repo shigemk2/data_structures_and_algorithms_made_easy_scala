4dd69606b7cd49081ce359bf027946e787ec81be
shigemk2 <i.am.shige@gmail.com>
2015-04-20 22:45:35 +0900

    Add 1-35 Scala

---
+++ chapter01/1-35.scala
@@ -0,0 +1,10 @@
+// C版の完全な移植は不可能
+object one35 {
+  def function(n: Int) {
+    for(i <- 1 to n; j <- 1 to n if j + i <= n) printf("i: %d, j: %d\n", i, j)
+  }
+
+  def main(args: Array[String]) {
+    function(8)
+  }
+}
