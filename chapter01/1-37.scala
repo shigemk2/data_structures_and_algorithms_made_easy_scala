08da7a5b23a6c689a38adce0e9d8edb958592e63
shigemk2 <i.am.shige@gmail.com>
2015-04-22 00:17:12 +0900

    Fix 1-37 動くようにした

--- chapter01/1-37.scala
+++ chapter01/1-37.scala
@@ -1,14 +1,15 @@
 object one37 {
   def function(n: Int): Int = {
-    for(1 to 3) {
+    for(i <- 1 to 3) {
       n match {
         case n if n <= 1 => 0
         case n => {
           function(3/n)
-          // printf("i: %d\n", i)
+          println(s"i: $i")
         }
       }
     }
+    0
   }
 
   def main(args: Array[String]) {
